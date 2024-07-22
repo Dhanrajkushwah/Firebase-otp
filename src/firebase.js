91cdd543d8874c1bd3e627bc36a0afd8037c54fa
Dhanraj Kushwah <dhanraj.kushwah007@gmail.com>
2024-07-22 09:20:23 +0530

    first

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtgrafkMFBCfj16cqAvzYmcAg7GHRxdLY",
+  authDomain: "phone-auth-31bea.firebaseapp.com",
+  projectId: "phone-auth-31bea",
+  storageBucket: "phone-auth-31bea.appspot.com",
+  messagingSenderId: "329753725099",
+  appId: "1:329753725099:web:2183fe911d715c25cb4fee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
